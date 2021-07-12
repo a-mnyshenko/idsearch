83a798426b00cd4f7d21665e9e08e3bd93196e46
Arthur Mnyshenko <flushdevelopment@gmail.com>
2021-07-12 04:24:19 +0300

    Improve perfomance

--- binarySearch.js
+++ binarySearch.js
@@ -1,10 +1,10 @@
-const range = initRangeOfRandomNumbers(-25000000, 25000000, 128)
+const range = initRangeOfRandomNumbers(-333, 333, 12000000)
 
 console.time("Searching complete in:")
 let result = binarySearchProducts(range)
 console.timeEnd("Searching complete in:")
+
 console.log(result)
-console.log(range.sort((a, b) => a - b).reverse())
 
 function initRangeOfNumbers(from, to) {
   const range = Math.abs(from) + Math.abs(to)
@@ -17,7 +17,7 @@ function initRangeOfNumbers(from, to) {
 
 function initRangeOfRandomNumbers(from, to, length) {
   return [...Array(length)].map((n) => (
-    n = Math.floor(Math.random() * (to - from) + from))
+    n = Math.round(Math.random() * (to - from) + from))
   )
 }
 
@@ -29,57 +29,40 @@ function binarySearchProducts(arr) {
   const isEven = isNumberEven(arr.length)
 
   let mid = isEven ? arr.length / 2 - 1 : Math.floor(arr.length / 2)
-  let minValue, minValuePrev, maxValue, maxValueNext
-
-  function swapMin(lower, higher) {
-    let tmp = minValue
-    minValuePrev = lower > tmp && lower < minValuePrev ? lower : tmp
-    minValue = lower < minValue ? lower : minValue
-
-    if(higher < minValuePrev) {
-      minValuePrev = higher
-    }
-
-    if(!minValue) {
-      minValuePrev = higher
-      minValue = lower
-    }
-  }
-
-  function swapMax(higher, lower) {
-    let tmp = maxValueNext
-    maxValue = higher < tmp && higher > maxValue ? higher : tmp
-    maxValueNext = higher > maxValueNext ? higher : maxValueNext
-
-    if(lower > maxValue){
-      maxValue = lower
-    }
-
-    if(!maxValue) {
-      maxValueNext = higher
-      maxValue = lower
-    }
-  }
+  let min1, min2, max1, max2
 
   // Start iterate from middle of array l - left iterator, r - right iterator ------##------ | #-------------#
   for(l = mid, r = isEven ? mid + 1 : mid; l >= 0; --l, ++r) {
-    let leftLess = arr[l] < arr[r]
+    let lower   = arr[l] < arr[r] ? arr[l] : arr[r],
+        higher = arr[l] < arr[r] ? arr[r] : arr[l]
 
-    if([arr[l]] < minValuePrev || [arr[r]] < minValuePrev) {
-      leftLess ? swapMin(arr[l], arr[r]) : swapMin(arr[r], arr[l])
+    if(lower < min2) {
+      min2 = lower < min1 ? min1 : lower > min1 ? lower : min2
+      min1 = lower < min1 ? lower : min1
+  
+      if(higher < min2) {
+        min2 =  higher > min1 ? higher : min2
+      }
     }
 
-    if([arr[l]] > maxValue || [arr[r]] > maxValue) {
-      leftLess ? swapMax(arr[r], arr[l]) : swapMax(arr[l], arr[r])
+    if(higher > max1) {
+      max1 =  higher > max2 ? max2 :  higher < max2 ? higher : max1
+      max2 =  higher > max2 ? higher : max2
+
+      if(lower > max1) {
+        max1 = lower < max2 ? lower : max1
+      }
     }
 
-    if (!minValue && !maxValue) {
-       leftLess ? swapMin(arr[l], arr[r]) : swapMin(arr[r], arr[l])
-       leftLess ? swapMax(arr[r], arr[l]) : swapMax(arr[l], arr[r])
+    if(!min1 && !max1) {
+      min1 = lower, min2 =  higher
+      max1 = lower, max2 =  higher
     }
   }
 
-  console.log(minValue, minValuePrev, maxValue, maxValueNext)
+  const result = []
+  min2 < 0 && result.push(min1, min2)
+  max1 > 0 && result.push(max1, max2)
 
-  return
+  return result
 }
